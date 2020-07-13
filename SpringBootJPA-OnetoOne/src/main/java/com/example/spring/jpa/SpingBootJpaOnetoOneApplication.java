ab59d4f67c3b89f75688e7a8b969d70683134ebf
prakashananthan <ananthanprakash@gmail.com>
2020-07-13 21:01:34 +0530

    Initial Level

---
+++ SpringBootJPA-OnetoOne/src/main/java/com/example/spring/jpa/SpingBootJpaOnetoOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootJpaOnetoOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpingBootJpaOnetoOneApplication.class, args);
+    }
+
+}
---
+++ SpringBootJPA-OnetoOne/src/test/java/com/example/spring/jpa/SpingBootJpaOnetoOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingBootJpaOnetoOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
